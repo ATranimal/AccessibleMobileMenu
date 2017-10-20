9b14d78d77300dc3fab0497a43cb40c85a4ce32c
Thomas Oo <oo.thomas96@gmail.com>
2017-10-20 04:21:15 -0400

    Preferences ticks are persistent

--- src/site/site.js
+++ src/site/site.js
@@ -2,15 +2,18 @@ var express = require('express'),
   router = express.Router(),
   menu = require('../controllers/dish'),
   mongoose = require('mongoose'),
-  Dish = mongoose.model('Dishes');
+  Dish = mongoose.model('Dishes'),
+  _ = require('underscore');
 
 router.route('/')
   .get(function (req, res, next) {
     Dish.find({}, function(err, foodList) {
       // TODO: Proper error hadnling
       if (err) console.log("ERROR: Query cannot be found");
+      let categories = _.pluck(foodList, 'category');
       return res.render('pages/index', {
-        dishes: foodList
+        dishes: foodList,
+        categories: categories
       });
     });
   })
