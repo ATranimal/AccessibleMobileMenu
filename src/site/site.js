6164f9a18b43c03465eb5b1ad5fcbf3f694df8c1
ATranimal <tran.andrew.a@gmail.com>
2017-11-21 02:12:08 -0500

    Added preliminary food dating

--- src/site/site.js
+++ src/site/site.js
@@ -12,56 +12,57 @@ router.route('/')
   })
 
 
+router.route('/dating/')
+  .get(function (req, res, next) {
+    Dish.find({}, function(err, foodList) {
+      // TODO: Proper error hadnling
+      if (err) console.log("ERROR: Query cannot be found");
+      var sample = _.sample(foodList);
+      return res.render('pages/dating', {
+        dish: sample,
+      });
+    });
+  })
+
 router.route('/aw/')
-  .get(function (req, res, next) {
-    Dish.find({}, function(err, foodList) {
-      // TODO: Proper error hadnling
-      if (err) console.log("ERROR: Query cannot be found");
-      var categories = _.pluck(foodList, 'category');
-      categories = _.uniq(categories)
-      return res.render('pages/menu1', {
-        dishes: foodList,
-        categories: categories
-      });
+.get(function (req, res, next) {
+  Dish.find({}, function(err, foodList) {
+    // TODO: Proper error hadnling
+    if (err) console.log("ERROR: Query cannot be found");
+    var categories = _.pluck(foodList, 'category');
+    categories = _.uniq(categories)
+    return res.render('pages/menu1', {
+      dishes: foodList,
+      categories: categories
     });
-  })
-
-  router.route('/tran/')
-  .get(function (req, res, next) {
-    Dish.find({}, function(err, foodList) {
-      // TODO: Proper error hadnling
-      if (err) console.log("ERROR: Query cannot be found");
-      var categories = _.pluck(foodList, 'category');
-      categories = _.uniq(categories)
-      return res.render('pages/menu2', {
-        dishes: foodList,
-        categories: categories
-      });
-    });
-  })
+  });
+})
 
 
-  router.route('/3/')
-  .get(function (req, res, next) {
-    Dish.find({}, function(err, foodList) {
-      // TODO: Proper error hadnling
-      if (err) console.log("ERROR: Query cannot be found");
-      return res.render('pages/index3', {
-        dishes: foodList
-      });
+router.route('/tran/')
+.get(function (req, res, next) {
+  Dish.find({}, function(err, foodList) {
+    // TODO: Proper error hadnling
+    if (err) console.log("ERROR: Query cannot be found");
+    var categories = _.pluck(foodList, 'category');
+    categories = _.uniq(categories)
+    return res.render('pages/menu2', {
+      dishes: foodList,
+      categories: categories
     });
-  })
+  });
+})
 
 
 router.route('/inputform/')
-  .get(function (req, res, next) {
-    return res.render('pages/inputform');
-  });
+.get(function (req, res, next) {
+  return res.render('pages/inputform');
+});
 
 router.route('/preferences')
-  .get(function (req, res, next) {
-    return res.render('pages/preferences');
-  })
+.get(function (req, res, next) {
+  return res.render('pages/preferences');
+})
 
   
 module.exports = router;
